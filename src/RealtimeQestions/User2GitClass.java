cec2a1ece5324183f9e0c432163f8bd5b69c17ec
ADMIN <ADMIN@DESKTOP-3TL6T9R>
2024-03-19 20:51:49 +0530

    "User2 Git Class Added"

---
+++ src/RealtimeQestions/User2GitClass.java
@@ -0,0 +1,11 @@
+package RealtimeQestions;
+
+public class User2GitClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("User2 Git Class Added");
+	}
+
+}
